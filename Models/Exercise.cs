0dd1671bee70a5af06775f38c9d01b7ce6a55108
Yanaa88 <80061327+Yanaa88@users.noreply.github.com>
2024-11-19 21:33:31 +0200

    Added DbContext and Asp Identity

---
+++ Models/Exercise.cs
@@ -0,0 +1,15 @@
+﻿namespace YogaApplicationApi.Models
+{
+    public class Exercise
+    {
+        public string? Name { get; set; } 
+
+        public string? Difficulty { get; set; } 
+
+        public int Duration { get; set; }
+                
+        public string VideoUrl { get; set; } 
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using YogaApplicationApi;
+using YogaApplicationApi.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ConnectionStrings:DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,11 +31,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+   // app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapIdentityApi<User>();
 
 app.MapControllers();
 
